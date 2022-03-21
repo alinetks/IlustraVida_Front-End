953619561266627daafd78fa768ca5689084e879
Aline Lopes <97345449+alinetks@users.noreply.github.com>
2022-03-21 13:47:41 -0300

    Projeto React - parte 1 do blog

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Home.css';
+
+function Home(){
+    return(
+        <>
+            <h1 className="titulo"> Home </h1>
+            <img src="https://mir-s3-cdn-cf.behance.net/project_modules/1400/a0642f32096611.566f0e31225ed.jpg" alt="Imagem de tela inicial" className="img" />
+        </>
+    );
+}
+
+export default Home;
